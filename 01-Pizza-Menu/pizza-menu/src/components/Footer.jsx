04ef244979a963dbda30ca764213b24e49441780
Kazuhiro Takahashi <num96kazu19@icloud.com>
2023-06-20 23:28:50 +0900

    条件分岐とか。とりあえず第1プロジェクト終了

--- 01-Pizza-Menu/pizza-menu/src/components/Footer.jsx
+++ 01-Pizza-Menu/pizza-menu/src/components/Footer.jsx
@@ -5,16 +5,30 @@ const Footer = () => {
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
-  return (
-    <footer className="footer">
-      {isOpen && (
-        <div className="order">
-          <p>We are open from 12:00 to 22:00. Come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
-      )}
-    </footer>
-  );
+  if (!isOpen)
+    return (
+      <footer className="footer">
+        {isOpen ? (
+          <Order closeHour={closeHour} openHour={openHour} />
+        ) : (
+          <p>
+            We are happy to welcome you between {openHour}:00 and {closeHour}:00
+          </p>
+        )}
+      </footer>
+    );
 };
 
 export default Footer;
+
+const Order = ({ closeHour, openHour }) => {
+  return (
+    <div className="order">
+      <p>
+        We are open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
+};
--- 01-Pizza-Menu/pizza-menu/src/components/Menu.jsx
+++ 01-Pizza-Menu/pizza-menu/src/components/Menu.jsx
@@ -3,17 +3,26 @@ import pizzaData from "../data";
 
 const Menu = () => {
   const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
 
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {pizzas && (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza key={pizza.name} pizzaObj={pizza} />
-          ))}
-        </ul>
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone even, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza key={pizza.name} pizzaObj={pizza} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We are still working on our menu. Please come back later.</p>
       )}
     </main>
   );
